322626779428e9621eeaa5a18b07f13a71b2147d
pkonnoth <67570190+pkonnoth@users.noreply.github.com>
2023-02-28 23:06:29 -0500

    app done

--- webserver/app.py
+++ webserver/app.py
@@ -1,63 +1,76 @@
-from flask import Flask, render_template, request, redirect, url_for
-import mysql.connector
-import config
+from flask import *
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 app = Flask(__name__)
-db = mysql.connector.connect(
-    host=config.db_host,
-    user=config.db_user,
-    password=config.db_password,
-    database=config.db_name
-)
+app.secret_key = 'mysecretkey'
 
+mysql = MySQL(app)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'users'
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return redirect(url_for('login'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password,))
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['id'] = user['id']
+            session['username'] = user['username']
+            return redirect(url_for('dashboard'))
+        else:
+            message = 'Invalid email or password'
+            return render_template('login.html', message=message)
+    else:
+        if 'loggedin' in session:
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
-        confirm_password = request.form['confirm_password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
+        user = cursor.fetchone()
+        if user:
+            message = 'Email already exists'
+            return render_template('register.html', message=message)
+        else:
+            cursor.execute('INSERT INTO users VALUES (NULL, %s, %s, %s)', (username, email, password,))
+            mysql.connection.commit()
+            message = 'You have successfully registered'
+            return render_template('register.html', message=message)
+    else:
+        return render_template('register.html')
 
-        if password != confirm_password:
-            return "Passwords do not match"
-
-        cursor = db.cursor()
-        query = "INSERT INTO users (username, password) VALUES (%s, %s)"
-        values = (username, password)
-        cursor.execute(query, values)
-        db.commit()
-        cursor.close()
-
+@app.route('/dashboard')
+def dashboard():
+    if 'loggedin' in session:
+        return render_template('dash.html')
+    else:
         return redirect(url_for('login'))
 
-    return render_template('register.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        cursor = db.cursor()
-        query = "SELECT * FROM users WHERE username = %s AND password = %s"
-        values = (username, password)
-        cursor.execute(query, values)
-        user = cursor.fetchone()
-        cursor.close()
-
-        if user:
-            return "Welcome, " + user[1]
-        else:
-            return "Invalid username or password"
-
-    return render_template('login.html')
-
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- webserver/config.py
+++
@@ -1,6 +0,0 @@
-
-
-db_host = 'localhost'
-db_user = 'root'
-db_password = 'Irine2012@'
-db_name = 'mydatabase'
